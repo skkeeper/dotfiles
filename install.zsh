6d23848ec1104ab2ce1aba437eb3ece17a2ba053
Fábio André Damas <skkeeper@gmail.com>
2020-05-17 11:16:06 +0100

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -27,6 +27,8 @@ cd "$startDir"
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm ~/.zshrc.pre-oh-my-zsh
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
